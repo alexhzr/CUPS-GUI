4615aeac2151dbbb8964bec735346017f59cf98e
Franciscorodr <fcorodr@gmail.com>
2015-01-28 16:25:35 +0100

    Fixed jquery scripts
    Added "$(document).ready()" to scripts.js
    
    Added link to scripts.js after it was deleted (Luis did it)

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,14 +1,17 @@
-$("#policy-menu li").draggable({
-	helper: "clone"
-});
-$(".policy-statements ul").droppable({
-	drop: function( event, ui ) {
-		$( "<li></li>" ).html( ui.draggable.text() + "<input type='button' onclick='deleteItem(this)' value='Borrar'/>" ).appendTo( this );
-	}
-});
+$(document).ready( function() {
+	$("#policy-menu li").draggable({
+		helper: "clone"
+	});
+	$(".policy-statements ul").droppable({
+		drop: function( event, ui ) {
+			$( "<li></li>" ).html( ui.draggable.text() + "<input type='button' onclick='deleteItem(this)' value='Borrar'/>" ).appendTo( this );
+		}
+	});
+	$( "button" ).click(function() {
+	 $( ".policy-statements" ).show( "slow" );
+	});
+})
+
 function deleteItem(item) {
 	$(item).parent().remove();
 }
-$( "button" ).click(function() {
- $( ".policy-statements" ).show( "slow" );
-});
