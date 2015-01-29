9bb810c29b6ea190315fa55d94644a9dfb9a730d
Franciscorodr <fcorodr@gmail.com>
2015-01-29 20:19:13 +0100

    Fixed code from scripts.js
    Some methods and functions weren't properly closed

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -2,13 +2,13 @@ $(document).ready( function() {
 	$("#policy-menu li").draggable({
 		helper: "clone"
 	});
-	
+});
 $(".policy-statements ul").droppable({
 		drop: function( event, ui ) {
 			$( "<li></li>" ).html( ui.draggable.text() + "<input type='button' onclick='deleteItem(this)' value='Borrar'/>" ).appendTo( this );
 	}
 
-})
+});
 /*Oculta el hide por el cual el id será igual que el que le pase por la funcion onclick*/
 function hideDiv(id) {
 	$("#"+id).hide("slow");
