0c90962e9354585debf72396868aadfd2b932159
ManriqueDJ <luis.fco.om@gmail.com>
2015-01-29 14:38:25 +0100

    Added header to the structure

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -2,18 +2,21 @@ $(document).ready( function() {
 	$("#policy-menu li").draggable({
 		helper: "clone"
 	});
-	$(".policy-statements ul").droppable({
+	
+$(".policy-statements ul").droppable({
 		drop: function( event, ui ) {
 			$( "<li></li>" ).html( ui.draggable.text() + "<input type='button' onclick='deleteItem(this)' value='Borrar'/>" ).appendTo( this );
-		}
-	});
-	$( "button" ).click(function() {
-		$( ".policy-statements" ).show( "slow" );
-	});
-	$( ".hide" ).click(function() {
-		$( ".policy-statements" ).hide( "slow" );
-	});
+	}
+
 })
+/*Oculta el hide por el cual el id será igual que el que le pase por la funcion onclick*/
+function hideDiv(id) {
+	$("#"+id).hide("slow");
+}
+
+function showDiv(id) {
+	$("#"+id).show("slow");
+}
 
 function deleteItem(item) {
 	$(item).parent().remove();
